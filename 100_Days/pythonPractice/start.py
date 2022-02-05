72996746844d74a11eda20d8de8282fd4d9c4c8a
BRANDDY <57237646+BRANDDY@users.noreply.github.com>
2022-02-05 14:58:24 -0500

    start with python

---
+++ 100_Days/pythonPractice/start.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+str='123456789'
+
+print(str) # whole string of str
+print(str[0: -1]) # from the first to the last two
+print(str[0]) #the first
+print(str[2:5]) #from the third to the five
+print(str[2:]) #from third to end
+print(str[1:5:2]) ##### from the second to the one after next untill the fivith
+print(str*2) #print two times
+print(str + 'hello') #print both str and hello
+print('hello\nhi\n')
+print(r'hello\nhi')
